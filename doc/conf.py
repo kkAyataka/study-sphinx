548fd4a40531c31216e3299a881c4c0f70ec55e4
kkAyataka <kk.ayataka@gmail.com>
2019-07-13 10:26:41 +0900

    Adjust version...

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, kkAyataka'
 author = 'kkAyataka'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
