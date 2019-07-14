1943f5141029977540aab1677d5c3419c9a27e5c
kkAyataka <kk.ayataka@gmail.com>
2019-07-14 21:03:49 +0900

    Remove style-guide page from latexpdf
    - latexpdf does not support nested table

--- doc/conf.py
+++ doc/conf.py
@@ -72,3 +72,10 @@ html_style = 'css/rtd_custom.css'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for LaTeX -------------------------------------------------------
+
+latex_docclass = {'manual': 'jsbook'}
+
+if 'latexpdf' in sys.argv:
+    exclude_patterns.append('guide/style-guide.rst')
