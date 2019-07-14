01e870053e5f805a2a1e9a4d756ebcaaa6efd5a5
kkAyataka <kk.ayataka@gmail.com>
2019-07-14 20:35:39 +0900

    Define document version

--- doc/conf.py
+++ doc/conf.py
@@ -23,6 +23,7 @@ author = 'kkAyataka'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
+version = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
